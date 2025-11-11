ac7cc075f162f0410b7b795be9e0c5565b764996
bienfait shomari <bienfaitshm@gmail.com>
2025-11-11 12:29:22 +0200

    refactor(ui): refactor code of view

--- components/panels/__init__.py
+++ components/panels/__init__.py
@@ -1,6 +1,6 @@
 import wx
 
-from typing import List, Protocol, Any, Dict
+from typing import List, Protocol, Any, Dict, TypedDict
 from .about import AboutPanel
 from .course_management import CourseManagementPanel
 from .calculation import CalculationPanel
@@ -8,9 +8,26 @@ from .report import ReportPanel
 from components.dialog.cours_edit import CourseEditDialog
 
 
-class BasePanel(Protocol):
+class BasePanel(wx.Frame):
     def __init__(self, parent: wx.Notebook, controller: Any):
         pass
+
+
+class PanelObj:
+    def __init__(self, panel: BasePanel, name: str, title: str):
+        self.panel = panel
+        self.name = name
+        self.title = title
+
+
+PANELS: List[PanelObj] = [
+    PanelObj(name="courses", panel=CourseManagementPanel,
+             title="📚 Gestion des Cours"),
+    PanelObj(name="calculation", panel=CalculationPanel,
+             title="🧮 Calcul & Optimisation"),
+    PanelObj(name="report", panel=ReportPanel, title="📈 Rapport & Marge"),
+    PanelObj(name="about", panel=AboutPanel, title="ℹ️ À Propos"),
+]
 
 
 class PointAllocatorFrame(wx.Frame):
@@ -30,17 +47,22 @@ class PointAllocatorFrame(wx.Frame):
         self.notebook.Bind(wx.EVT_NOTEBOOK_PAGE_CHANGED, self._on_tab_change)
 
         # Composition des panneaux
-        self.management_panel = CourseManagementPanel(
-            self.notebook, controller)
-        self.calculation_panel = CalculationPanel(self.notebook, controller)
-        self.report_panel = ReportPanel(self.notebook, controller)
-        self.about_panel = AboutPanel(self.notebook, controller)
+        for panel in PANELS:
+            frame_panel = panel.panel(self.notebook, controller)
+            setattr(self, panel.name, frame_panel)
+            self.notebook.AddPage(frame_panel, panel.title)
 
-        self.notebook.AddPage(self.management_panel, "📚 Gestion des Cours")
-        self.notebook.AddPage(self.calculation_panel,
-                              "🧮 Calcul & Optimisation")
-        self.notebook.AddPage(self.report_panel, "📈 Rapport & Marge")
-        self.notebook.AddPage(self.about_panel, "ℹ️ À Propos")
+        # self.management_panel = CourseManagementPanel(
+        #     self.notebook, controller)
+        # self.calculation_panel = CalculationPanel(self.notebook, controller)
+        # self.report_panel = ReportPanel(self.notebook, controller)
+        # self.about_panel = AboutPanel(self.notebook, controller)
+
+        # self.notebook.AddPage(self.management_panel, "📚 Gestion des Cours")
+        # self.notebook.AddPage(self.calculation_panel,
+        #                       "🧮 Calcul & Optimisation")
+        # self.notebook.AddPage(self.report_panel, "📈 Rapport & Marge")
+        # self.notebook.AddPage(self.about_panel, "ℹ️ À Propos")
 
         main_sizer.Add(self.notebook, 1, wx.EXPAND | wx.ALL, 10)
         main_panel.SetSizer(main_sizer)
@@ -75,9 +97,15 @@ class PointAllocatorFrame(wx.Frame):
         dlg.Destroy()
 
     def _on_tab_change(self, event):
-        if event.GetSelection() == 0:
-            self.management_panel.update_display(self.controller.courses)
+        if event.GetSelection() == 0 and hasattr(self, "courses"):
+            getattr("courses").update_display(self.controller.courses)
         event.Skip()
+
+    def _update_panel(self, name: str):
+        def updater(*args, **kwargs):
+            if hasattr(self, name):
+                getattr(self, name).update_display(*args, **kwargs)
+        return updater
 
     def update_all_views(self, courses: List[Dict[str, Any]],
                          distributed_scores: Dict[str, int] = None,
@@ -85,7 +113,7 @@ class PointAllocatorFrame(wx.Frame):
         """Met à jour tous les panneaux avec les nouvelles données."""
 
         # L'onglet 1 (Gestion) est toujours mis à jour
-        self.management_panel.update_display(courses)
+        self._update_panel(name="courses")(courses)
 
         if final_results:
             # Si nous avons des résultats de calcul, mettre à jour les autres onglets
@@ -96,14 +124,21 @@ class PointAllocatorFrame(wx.Frame):
                 'optimal_mode_name', self.calculation_panel.get_rounding_mode())
             ignore_constraints = self.calculation_panel.get_ignore_constraints()
 
-            self.calculation_panel.update_display(
+            self._update_panel("calculation")(
                 courses, distributed_scores, final_results, total_weight,
                 target_percentage, final_mode_name, ignore_constraints
             )
+            # self.calculation_panel.update_display(
+            #     courses, distributed_scores, final_results, total_weight,
+            #     target_percentage, final_mode_name, ignore_constraints
+            # )
 
-            self.report_panel.update_display(
-                final_results, final_mode_name, ignore_constraints
-            )
+            self._update_panel("report")(
+                final_results, final_mode_name, ignore_constraints)
+
+            # # self.report_panel.update_display(
+            #     final_results, final_mode_name, ignore_constraints
+            # # )
 
             # Basculer vers l'onglet des résultats
             self.notebook.SetSelection(1)
